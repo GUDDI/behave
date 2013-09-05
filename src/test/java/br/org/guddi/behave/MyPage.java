46d4a0bcbe5b60024a4cec310afb8647d12b07ff
PauloGladson <PauloGladson@escritorioCasa>
2013-09-04 23:27:37 -0300

    Colocação do projeto no openshift

---
+++ src/test/java/br/org/guddi/behave/MyPage.java
@@ -0,0 +1,18 @@
+package br.org.guddi.behave;
+
+import br.gov.frameworkdemoiselle.behave.annotation.ElementLocatorType;
+import br.gov.frameworkdemoiselle.behave.annotation.ElementMap;
+import br.gov.frameworkdemoiselle.behave.annotation.ScreenMap;
+import br.gov.frameworkdemoiselle.behave.runner.ui.Button;
+import br.gov.frameworkdemoiselle.behave.runner.ui.TextField;
+
+@ScreenMap(name = "Tela Inicial", location = "http://exemplo-guddi.rhcloud.com/guddi/")
+public class MyPage {
+
+	@ElementMap(name = "Campo de Busca", locatorType = ElementLocatorType.Id, locator = "busca")
+	private TextField searchField;
+
+	@ElementMap(name = "PESQUISAR", locatorType = ElementLocatorType.Id, locator = "search_button")
+	private Button buttonLuckSearch;
+
+}
---
+++ src/test/java/br/org/guddi/behave/MySteps.java
@@ -0,0 +1,8 @@
+package br.org.guddi.behave;
+
+import br.gov.frameworkdemoiselle.behave.parser.Step;
+import br.gov.frameworkdemoiselle.behave.parser.jbehave.CommonSteps;
+
+public class MySteps extends CommonSteps implements Step {
+
+}
