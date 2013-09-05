98a8b2b67b4df288eb8b3e2d6b2cbd0fca3fb3ea
PauloGladson <PauloGladson@escritorioCasa>
2013-09-05 19:28:17 -0300

    Colocação do projeto no openshift

--- src/test/java/behave/MySteps.java
+++ src/test/java/behave/MySteps.java
@@ -2,8 +2,6 @@ package behave;
 
 import br.gov.frameworkdemoiselle.behave.parser.Step;
 import br.gov.frameworkdemoiselle.behave.parser.jbehave.CommonSteps;
-import org.jbehave.core.annotations.Pending;
-import org.jbehave.core.annotations.When;
 
 public class MySteps extends CommonSteps implements Step {
 
